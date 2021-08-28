aadfa6853c0947ed3688a4796de4c3f711ad77b0
MerleYuridia <2203026575@correo.ler.uam.mx>
2021-08-27 23:16:20 -0500

    Hola mundo

---
+++ main.c
@@ -0,0 +1,21 @@
+/*comentarios para nosotros en el futuro 
+nombre del programa 
+Autor 
+Modificado por 
+que hace el programa 
+necesita un circuito
+colocar una imagen en el repositorio_liga 
+terminales que se utilizan, el modo 
+ejemplo: gpio 5_salida*/
+
+/*Bibliotecas*/
+
+#include <stdio.h>
+
+int  main () 
+{
+    printf ( " Hola mundo\n" );
+    return  0 ;
+}
+
+
